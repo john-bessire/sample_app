65eb9856eb84e04fe4aacffc3feea18a265067ed
John Bessire <johnb10000@yahoo.com>
2012-05-02 17:41:29 -0700

    Fix routing problems

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,21 +1,14 @@
 class StaticPagesController < ApplicationController
-  
   def home
-    @title = "Home"
   end
 
-  def contact
-    @title = "Contact"
-  end
-
-  def about
-    @title = "About"
+  def help
   end
 
   def about
-    @title = "help"
   end
 
   def contact
   end
+
 end
